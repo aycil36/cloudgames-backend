c9cf754d309f28ecbbd5f162fcee019e8f46ce44
Air <air@Air-MacBook-Air.local>
2025-04-16 19:40:36 +0300

    comments schema düzeltildi

--- models/game.js
+++ models/game.js
@@ -9,18 +9,21 @@ const gameSchema = new mongoose.Schema({
   isRatingEnabled: Boolean,
   releaseDate: String,
   developer: String,
-  comments: [
-    {
-      userId:mongoose.Schema.Types.ObjectId,
-      userName: String,
-      text: String,
-      playTime: Number,
-      rating: {
-        type: Number,
-        default:0
+  comments: {
+    type: [
+      {
+        userId: mongoose.Schema.Types.ObjectId,
+        userName: String,
+        text: String,
+        playTime: Number,
+        rating: {
+          type: Number,
+          default: 0
+        }
       }
-    }
-  ]
+    ],
+    default: []
+  }
   
 
 });
